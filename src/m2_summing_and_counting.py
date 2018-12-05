fbff92b340f90787d55088227d8fac19cdfea641
jneill775 <neilljs@rose-hulman.edu>
2018-12-05 12:05:51 -0500

    part 2 done

--- src/m2_summing_and_counting.py
+++ src/m2_summing_and_counting.py
@@ -222,7 +222,7 @@ def count_sines_from(m, n):
 def run_test_count_sines_vs_cosines():
     """ Tests the   count_sines_vs_cosines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  count_sines_vs_cosines  function defined below.
     #   Include at least **   6   ** tests (we wrote one for you).
     #              ** Yes, 6 (six) tests. **
@@ -241,7 +241,7 @@ def run_test_count_sines_vs_cosines():
     print('       actual:  ', answer)
 
     # -------------------------------------------------------------------------
-    # TODO: 6 (continued).
+    # DONE: 6 (continued).
     # Below this comment, add 5 more test cases of your own choosing.
     # -------------------------------------------------------------------------
 
@@ -267,8 +267,8 @@ def count_sines_vs_cosines(m):
 
     total = 0
 
-    for k in range(2 * m):
-        if math.sin(m) > math.cos(m):
+    for k in range((2 * m) + 1):
+        if math.sin(-m + k) > math.cos(-m + k):
             total = total + 1
     return total
 
@@ -297,7 +297,7 @@ def count_sines_vs_cosines(m):
       -- Also:  count_sines_vs_cosines(101) returns 100 (trust me!)
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPORTANT: As in previous problems in this session,
